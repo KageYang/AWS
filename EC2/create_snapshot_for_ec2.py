a11920ea3580d8dae9f09eb06cc492ead5626833
KageYang <kaichih.yang@gmail.com>
2022-02-15 11:19:17 +0800

    Create create_snapshot_for_ec2.py

---
+++ EC2/create_snapshot_for_ec2.py
@@ -0,0 +1,23 @@
+import boto3
+from boto.ec2 import connect_to_region
+
+conn = connect_to_region('ap-northeast-1')
+
+# Command Line input EC2 Instance ID
+instance = input('InstanceID : ')
+
+ec2_resource = boto3.resource('ec2')
+
+# Filter Designated EC2 Instance ID
+for instance in ec2_resource.instances.filter(
+	        InstanceIds=[
+     					  instance
+						],
+	):
+# Create Snapshot for all EBS volumes attached to this EC2 Instance
+    for volume in instance.volumes.all():
+    	print(volume.id)	
+    	snapshot = ec2_resource.create_snapshot(
+    		VolumeId=volume.id
+    )
+    	print(f'Snapshot {snapshot.id} created for volume {volume.id}')
